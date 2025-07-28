9eff367161b7db268f1ccc0b775d5a1ddf5ab8dd
nitziacovarrubias <nitzia260@gmail.com>
2025-07-28 14:43:59 -0400

    formatting json response from emotion_detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,7 +7,29 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     response = requests.post(url, json=obj, headers=header)
-    formatted_response = json.loads(response.text)
+    json_response = json.loads(response.text)
+
+    emotions = json_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = {'emotion': None, 'score': 0}
+    
+    for emotion, score in emotions.items():
+        if(score > dominant_emotion['score']):
+            dominant_emotion['score'] = score
+            dominant_emotion['emotion'] = emotion
+
+    formatted_response = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion['emotion']
+    }
 
     return formatted_response 
 
